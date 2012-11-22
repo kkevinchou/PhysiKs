ce669021dfb45ea66c1609c4fc0ae024826dcead
Kevin C <kkevinchou@gmail.com>
2012-11-22 01:05:18 -0800

    more colors stuff

--- src/physiks/visual/DaVinci.java
+++ src/physiks/visual/DaVinci.java
@@ -16,17 +16,15 @@ public class DaVinci {
 		
 		// Color palette, should move this to a separate static class later
 		colors = new ArrayList<Color>();
-		colors.add(new Color(245, 162, 63));
-		colors.add(new Color(168, 112, 44));
-		colors.add(new Color(109, 190, 194));
-		colors.add(new Color(255, 245, 81));
-		colors.add(new Color(215, 20, 20));
-		colors.add(new Color(225, 123, 10));
-		colors.add(new Color(102, 51, 84));
-		colors.add(new Color(174, 33, 67));
-		colors.add(new Color(51, 51, 85));
-		colors.add(new Color(17, 68, 85));
-		colors.add(new Color(7, 155, 123));
+		colors.add(new Color(255, 226, 0));
+		colors.add(new Color(0, 216, 57));
+		colors.add(new Color(255, 35, 0));
+		colors.add(new Color(81, 6, 206));
+		colors.add(new Color(161, 103, 0));
+		colors.add(new Color(255, 57, 0));
+		colors.add(new Color(253, 115, 157));
+		colors.add(new Color(249, 255, 208));
+		colors.add(new Color(2, 40, 109));
 	}
 	
 	public Color getRandomColor() {
--- src/physiks/visual/RenderEngine.java
+++ src/physiks/visual/RenderEngine.java
@@ -2,16 +2,32 @@ package physiks.visual;
 
 import java.util.List;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.geom.Polygon;
+import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.ShapeRenderer;
 
+import physiks.PhysiKsSim;
 import physiks.entities.*;
 import physiks.geometry.Vector2D;
 
 public class RenderEngine {
+	private Color backgroundColor;
+	private Polygon background;
 	private List<RigidBody> entities;
 	
 	public RenderEngine(List<RigidBody> rigidBodies) {
+		backgroundColor = new Color(187, 234, 239);
+		background = new Polygon();
+		
+		background.addPoint(0, 0);
+		background.addPoint(PhysiKsSim.WIDTH, 0);
+		background.addPoint(PhysiKsSim.WIDTH, PhysiKsSim.HEIGHT);
+		background.addPoint(0, PhysiKsSim.HEIGHT);
+		background.setClosed(true);
+		
 		this.entities = rigidBodies;
 	}
 	
@@ -32,8 +48,13 @@ public class RenderEngine {
 	}
 	
 	public void update(Graphics graphics) {
+		graphics.setColor(backgroundColor);
+		ShapeRenderer.fill(background);
+
 		for (RigidBody body : entities) {
 			drawBody(body, graphics);
 		}
+		graphics.setColor(backgroundColor);
+		graphics.draw(background);
 	}
 }
