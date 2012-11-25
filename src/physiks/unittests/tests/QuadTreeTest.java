f7f220adaec024af4bd31b45fe3d6f0b0cb958f2
Kevin C <kkevinchou@gmail.com>
2012-11-25 00:38:26 -0800

    quadtree unit test cleanup

--- src/physiks/unittests/tests/QuadTreeTest.java
+++ src/physiks/unittests/tests/QuadTreeTest.java
@@ -18,20 +18,14 @@ public class QuadTreeTest extends Test {
 	@Override
 	public void run() {
 		fractionalPositionTest();
-		basicIntersectionTest();
+		boundaryTest();
 		bodyIntersectionTest();
 	}
 	
-	private void basicIntersectionTest() {
+	private void boundaryTest() {
 		QuadTree q = new QuadTree(0, 0, 5, 5);
 		
-		List<Vector2D> points = new ArrayList<Vector2D>();
-		points.add(new Vector2D(0, 0));
-		points.add(new Vector2D(1, 0));
-		points.add(new Vector2D(1, 1));
-		points.add(new Vector2D(0, 1));
-		
-		PolyBody body = new PolyBody(0, 0, 1, points);
+		PolyBody body = new PolyBody(0, 0, 2, 2);
 		
 		body.setPosition(new Vector2D(0, 0));
 		tAssert(q.intersects(body), true);
@@ -64,17 +58,11 @@ public class QuadTreeTest extends Test {
 	private void bodyIntersectionTest() {
 		QuadTree q = new QuadTree(0, 0, 5, 5);
 		
-		List<Vector2D> points = new ArrayList<Vector2D>();
-		points.add(new Vector2D(0, 0));
-		points.add(new Vector2D(1, 0));
-		points.add(new Vector2D(1, 1));
-		points.add(new Vector2D(0, 1));
-		
-		PolyBody body1 = new PolyBody(2, 0, 1, points); // node 0
-		PolyBody body2 = new PolyBody(0, 0, 1, points); // node 1
-		PolyBody body3 = new PolyBody(0, 2, 1, points); // node 2
-		PolyBody body4 = new PolyBody(3, 3, 1, points); // node 3
-		PolyBody body5 = new PolyBody(3, 3, 1, points); // node 3
+		PolyBody body1 = new PolyBody(2, 0, 2, 2); // node 0
+		PolyBody body2 = new PolyBody(0, 0, 2, 2); // node 1
+		PolyBody body3 = new PolyBody(0, 2, 2, 2); // node 2
+		PolyBody body4 = new PolyBody(3, 3, 2, 2); // node 3
+		PolyBody body5 = new PolyBody(3, 3, 2, 2); // node 3
 		
 		q.add(body1);
 		q.add(body2);
@@ -82,7 +70,7 @@ public class QuadTreeTest extends Test {
 		q.add(body4);
 		q.add(body5);
 		
-		PolyBody testBody = new PolyBody(0, 0, 1, points); // node 0
+		PolyBody testBody = new PolyBody(0, 0, 2, 2); // node 0
 		List<RigidBody> candidates;
 		
 		testBody.setPosition(new Vector2D(3, 0));
